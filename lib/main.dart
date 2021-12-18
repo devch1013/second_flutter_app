a74bbddfab42673ee5d77bdd0c20b563e96adff3
devch1013 <devch1013@gmail.com>
2021-12-18 11:20:50 +0900

    reset file

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,23 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+        ),
       ),
       home: MyHomePage(),
     );
@@ -26,24 +43,24 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't3',
-      title: 'New Clothes',
-      amount: 56.11,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: 't1',
+    //   title: 'New Shoes',
+    //   amount: 69.99,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Weekly Groceries',
+    //   amount: 16.53,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't3',
+    //   title: 'New Clothes',
+    //   amount: 56.11,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   void _addNewTransaction(String title, double amount) {
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,59 +10,74 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        //화면에 보일때만 요소 만듬
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color:
-                          Theme.of(context).primaryColor, // const value가 될 수 없음
-                      width: 2,
-                    ),
-                  ),
-                  //////////////////////////// 가격
-                  child: Text(
-                    '\$${_userTransactions[index].amount.toStringAsFixed(2)}', //소수점 둘째자리까지
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).primaryColor,
-                      fontSize: 20,
-                    ),
-                  ),
+      child: _userTransactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  'No transactions added yet',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    ////////////////////////// 제목
-                    Text(
-                      _userTransactions[index].title,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                    /////////////////////////날짜
-                    Text(
-                      DateFormat.yMMMEd().format(_userTransactions[index].date),
-                      style: TextStyle(color: Colors.grey),
-                    )
-                  ],
-                )
+                // 사이간격
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ))
               ],
+            )
+          : ListView.builder(
+              //화면에 보일때만 요소 만듬
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context)
+                                .primaryColor, // const value가 될 수 없음
+                            width: 2,
+                          ),
+                        ),
+                        //////////////////////////// 가격
+                        child: Text(
+                          '\$${_userTransactions[index].amount.toStringAsFixed(2)}', //소수점 둘째자리까지
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          ////////////////////////// 제목
+                          Text(_userTransactions[index].title,
+                              style: Theme.of(context).textTheme.headline6),
+                          /////////////////////////날짜
+                          Text(
+                            DateFormat.yMMMEd()
+                                .format(_userTransactions[index].date),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+              itemCount: _userTransactions.length,
             ),
-          );
-        },
-        itemCount: _userTransactions.length,
-      ),
     );
   }
 }
